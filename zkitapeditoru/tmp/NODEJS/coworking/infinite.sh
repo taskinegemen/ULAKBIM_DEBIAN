281f3b9044514d014f757299fa8bb6f4e2a7a9c5
Egemen <egementaskin@gmail.com>
2014-11-18 15:56:09 +0200

    relative path issue

--- zkitapeditoru/tmp/NODEJS/coworking/infinite.sh
+++ zkitapeditoru/tmp/NODEJS/coworking/infinite.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PATHOFFILE=`echo $DIR/cowork.js`  
-PATHTONODE=`which node`
+PATHOFFILE=`echo $DIR/cowork.js`
+
+PATHTONODE="node";
+if [ -f /usr/bin/node ]; then
+	PATHTONODE="/usr/bin/node"
+elif [ -f /usr/local/bin/node ]; then
+        PATHTONODE="/usr/local/bin/node"
+fi
+  
+#PATHTONODE=`which node`
 trap "echo Booh!" SIGINT SIGTERM
 while :
 do
